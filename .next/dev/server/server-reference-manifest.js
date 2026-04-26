1354a68e6b684289eb7a78757ed60849355ea09e
Yash Gaur <yashgaur@Yashs-MacBook-Air.local>
2026-04-26 16:22:16 +0530

    Remove .next from repo

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"PfN1FSk6U2+HuRRTwxCqy+pfnjuxOuJbtb0Nqj3/1e8=\"\n}"
--- .next/dev/static/chunks/src_app_cart_page_tsx_050yyod._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_0k~a3r.._.js","static/chunks/node_modules_0qz4w53._.js"],
-    source: "dynamic"
-});
